5aca13d4c0e221e1d9524cb134f9e4c29e703836
Vikramarka Mahendra <vikramarka9346@gmail.com>
2026-07-26 12:10:03 +0530

    Initialize Spring Boot application structure

--- 06-Spring-Boot-Web/WebApplication.java
+++ 06-Spring-Boot-Web/WebApplication.java
@@ -1,1 +1,13 @@
+package com.web.webApplication;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
+
+}
