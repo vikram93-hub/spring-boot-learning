b79aa04c324b7d84f25d961f6612eeb667623391
Vikramarka Mahendra <vikramarka9346@gmail.com>
2026-07-29 10:29:48 +0530

    Update ProductRepo.java

--- Spring-data-JPA/ProductRepo.java
+++ Spring-data-JPA/ProductRepo.java
@@ -1,1 +1,10 @@
+package com.web.webApplication.Repository;
+
+import com.web.webApplication.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product, Integer> {
+}
 
