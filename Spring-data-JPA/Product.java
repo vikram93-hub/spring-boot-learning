8f9a4168a7dca73da5138fc09a7e41ce6c50181c
Vikramarka Mahendra <vikramarka9346@gmail.com>
2026-07-29 10:28:46 +0530

    Update Product.java

--- Spring-data-JPA/Product.java
+++ Spring-data-JPA/Product.java
@@ -1,1 +1,14 @@
+package com.web.webApplication.model;
 
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.Id;
+
+@Entity
+
+public class Product {
+    @Id
+    private int prodId;
+    private String prodName;
+    private int price;
+}
