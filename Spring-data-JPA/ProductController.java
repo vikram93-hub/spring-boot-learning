36108b05973cec0830b45be085e9dcdad4b6b090
Vikramarka Mahendra <vikramarka9346@gmail.com>
2026-07-29 10:30:28 +0530

    Update ProductController.java

--- Spring-data-JPA/ProductController.java
+++ Spring-data-JPA/ProductController.java
@@ -1,1 +1,27 @@
+package com.web.webApplication.Contoller;
 
+import com.web.webApplication.model.Product;
+import com.web.webApplication.Service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductService service;
+
+    @RequestMapping("/products")
+    public List<Product> getProducts() {
+        return service.getProducts();
+    }
+    @RequestMapping("/products/{prodId}")
+    public Product getProductById(@PathVariable int prodId){
+        return service.getProductById(prodId);
+
+    }
+}
